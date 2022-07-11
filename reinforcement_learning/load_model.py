9a1a9a4701c46a2ad422abb502a6161a8dcd2607
sattyorin <sattyorin.sattyon@gmail.com>
2022-07-11 20:19:59 +0900

    added render

--- reinforcement_learning/load_model.py
+++ reinforcement_learning/load_model.py
@@ -1,7 +1,15 @@
 import gym
+import numpy as np
 
 import envs
 
 if __name__ == "__main__":
     env_id = "linear_actuator_array-v0"
     env = gym.make(env_id)
+
+    env.reset()
+    for _ in range(5):
+        for _ in range(100):
+            env.render()
+            env.step(env.action_space.sample())
+        env.reset()

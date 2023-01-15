2753cb76c5a98c31d4670df0b18aec258f4e8c56
sattyorin <sattyorin.sattyon@gmail.com>
2023-01-16 01:25:00 +0900

    added stir_ros files

---
+++ ros/stir_ros/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="stir_ros",
+    version="0.0.0",
+    description="stir ros",
+    author="sattyorin",
+    author_email="sattyorin.sattyon@gmail.com",
+    license="MIT License",
+    packages=["stir_ros"],
+    package_dir={"stir_ros": "src/stir_ros"},
+)

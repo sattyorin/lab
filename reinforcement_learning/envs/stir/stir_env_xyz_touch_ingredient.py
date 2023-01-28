7436ff111fe99a2a5e2f1027b381c93ab39cd9bb
sattyorin <sattyorin.sattyon@gmail.com>
2023-01-28 18:00:19 +0900

    added finish condition of small velocity

--- reinforcement_learning/envs/stir/stir_env_xyz_touch_ingredient.py
+++ reinforcement_learning/envs/stir/stir_env_xyz_touch_ingredient.py
@@ -7,7 +7,7 @@ import yaml
 from envs.stir.i_stir_env import IStirEnv
 from gym.spaces import Box
 
-_TARGET_VELOCITY = 0.08
+_TARGET_VELOCITY = 0.02
 _THRESHOLD_DISTANCE = 0.01
 
 
@@ -78,10 +78,10 @@ class StirEnvXYZTouchIngredients(IStirEnv):
 
     def _get_reward(self, observation: np.ndarray) -> Tuple[float, bool]:
         tool_pose = observation[: self._length_tool_pose]
-        # tool_velocity = observation[
-        #     self._length_tool_pose : self._length_tool_pose
-        #     + self._length_tool_velocity
-        # ]
+        tool_velocity = observation[
+            self._length_tool_pose : self._length_tool_pose
+            + self._length_tool_velocity
+        ]
         ingredient_positions = observation[
             self._length_tool_pose + self._length_tool_velocity :
         ]
@@ -104,6 +104,14 @@ class StirEnvXYZTouchIngredients(IStirEnv):
             )
             > 0.0000001
         ):
+            return 100, True
+
+        self._total_velocity_reward += stir_util.get_reward_small_velocity(
+            np.linalg.norm(tool_velocity[:3]), _TARGET_VELOCITY
+        )
+
+        if self._total_velocity_reward / (self.num_step + 1) < 0.5:
+            print(self._total_velocity_reward)
             return reward, True
 
         self._previous_ingredient_positions = ingredient_positions

a016fc211bd979896504494af97de88fe75fb7ee
sattyorin <sattyorin.sattyon@gmail.com>
2023-04-02 19:50:48 +0900

    make it simple

--- reinforcement_learning/plot_scores.py
+++ reinforcement_learning/plot_scores.py
@@ -1,62 +1,64 @@
 import argparse
+import glob
 import os
 
-# matplotlib.use("Agg")  # Needed to run without X-server
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 def main():
+    current_script_path = os.path.abspath(__file__)
+    directories = glob.glob(
+        os.path.join(os.path.dirname(current_script_path), "results", "*/")
+    )
+    latest_directory = max(directories, key=os.path.getctime)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--title", type=str, default="")
     parser.add_argument(
         "--file",
-        action="append",
-        dest="files",
-        default=[],
+        default=os.path.join(
+            "results", os.path.basename(os.path.normpath(latest_directory))
+        ),
         type=str,
-        help="specify paths of scores.txt",
-    )
-    parser.add_argument(
-        "--label",
-        action="append",
-        dest="labels",
-        default=[],
-        type=str,
-        help="specify labels for scores.txt files",
+        help="specify path of scores.txt",
     )
     parser.add_argument("--save", dest="save", default=False, type=bool)
     args = parser.parse_args()
 
-    assert len(args.files) > 0
-    assert len(args.labels) == len(args.files)
+    score_path = os.path.join(args.file, "scores.txt")
+    assert os.path.exists(score_path)
+    scores = pd.read_csv(score_path, delimiter="\t")
+    plt.plot(
+        scores["episodes"].to_numpy(),
+        scores["mean"].to_numpy(),
+        label="mean",
+        color="pink",
+        ls="-",
+    )
+    plt.plot(
+        scores["episodes"].to_numpy(),
+        scores["max"].to_numpy(),
+        label="max",
+        color="yellowgreen",
+        ls="--",
+    )
 
-    for fpath, label in zip(args.files, args.labels):
-        if os.path.isdir(fpath):
-            fpath = os.path.join(fpath, "scores.txt")
-        assert os.path.exists(fpath)
-        scores = pd.read_csv(fpath, delimiter="\t")
-        # plt.plot(scores["steps"], scores["mean"], label=label)
-        plt.plot(
-            scores["episodes"], scores["mean"], label=label, color="yellowgreen"
-        )
-        # plt.plot(scores["episodes"], scores["max"], label="max")
-
-    # plt.xlabel("steps")
     plt.xlabel("episodes")
-    plt.ylabel("total reward")
+    plt.ylabel("total reward mean")
     plt.legend(loc="best")
     if args.title:
         plt.title(args.title)
 
-    fig_fname = args.files[0] + "mean" + ".png"
-    fig_fname_svg = args.files[0] + "mean" + ".svg"
+    fig_fname_png = args.file + "mean" + ".png"
+    fig_fname_svg = args.file + "mean" + ".svg"
     if args.save:
-        plt.savefig(fig_fname)
+        plt.savefig(fig_fname_png)
         plt.savefig(fig_fname_svg)
+        print("Saved a figure as {}".format(fig_fname_png))
+        print("Saved a figure as {}".format(fig_fname_svg))
     else:
         plt.show()
-    print("Saved a figure as {}".format(fig_fname))
 
 
 if __name__ == "__main__":
